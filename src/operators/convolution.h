8febae9cfe54e56da39e9ee20cb1a383b08512d5
Christian Reinbacher <reinbacher@icg.tugraz.at>
2017-03-17 09:51:04 +0100

    added licence

--- src/operators/convolution.h
+++ src/operators/convolution.h
@@ -1,3 +1,20 @@
+// This file is part of cnn-crf-stereo.
+//
+// Copyright (C) 2017 Patrick Knöbelreiter <knoebelreiter at icg dot tugraz dot at>
+// Institute for Computer Graphics and Vision, Graz University of Technology
+// https://www.tugraz.at/institute/icg/teams/team-pock/
+//
+// dvs-panotracking is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or any later version.
+//
+// cnn-crf-stereo is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
 #include <cudnn.h>
 #include "iu/iucore.h"
